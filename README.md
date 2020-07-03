dbe9f67614a9c32740b457dacb39e459d00fd224
Joseph <josephmampillil@gmail.com>
2020-07-02 20:12:18 -0400

    Added Files

---
+++ README.md
@@ -0,0 +1,5 @@
+Joe Mampillil - 07.02.2020
+
+R Bootcamp from https://orion.tw.rpi.edu/
+
+I'm trying to learn R :)
---
+++ chapter1.R
@@ -0,0 +1,7 @@
+# joe mampillil - 07.02.2020
+# bootcamp chapter 1 - basics
+
+# numeric vector
+nums <- c(1.1, 3, -5.7)
+devs <- rnorm(5)
+devs
